68b80ae035fc7a4830fa3b8e6ddd9d2147fbbe51
_nullPointer <palpiyush6@gmail.com>
2019-05-01 22:17:18 +0530

    Insertion Sort implementation

---
+++ sorting/InsertionSort.java
@@ -0,0 +1,33 @@
+package sorting;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] ar = {5,2,4,1,3};
+        int[] sorted = insertionSort(ar);
+
+        for(int x:sorted){
+            System.out.println(x);
+        }
+    }
+
+    private static int[] insertionSort(int[] ar){
+        // start from i=1 to ar.length-1. i=0 element is considered as sorted
+        for(int i=1;i<ar.length;i++){
+            // pick an element
+            int element = ar[i];
+
+            // initialize j
+            int j = i-1;
+
+            // check inside while loop
+            while(j>=0 && ar[j] > element){
+                // swap part 1
+                ar[j+1] = ar[j];
+                j--;
+            }
+            // swap part 2
+            ar[j+1] = element;
+        }
+        return ar;
+    }
+}
